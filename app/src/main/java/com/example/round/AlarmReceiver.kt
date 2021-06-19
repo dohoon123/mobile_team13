3b0b2609bbccb07a5d1a51ee8a2abca9f866bc97
dohoon <65086850+dohoon123@users.noreply.github.com>
2021-06-20 02:26:17 +0900

    알람기능 달았습니다. 시간을 설정하면(avd기준) 종료시간이 되면 오래지나지 않아(alarmmanager로 구현하면 완전히 정확하게 울리지는 않는다고 합니다.) 알람이 오고 그걸 누르면 지정된 종료 시간과 누른 시간이 너무 차이나지 않으면 rDB의 alarmChecked값이 증가합니다

---
+++ app/src/main/java/com/example/round/AlarmReceiver.kt
@@ -0,0 +1,74 @@
+package com.example.round
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import java.time.LocalDateTime
+
+class AlarmReceiver : BroadcastReceiver() {
+
+    companion object {
+        const val TAG = "AlarmReceiver"
+        const val PRIMARY_CHANNEL_ID = "primary_notification_channel"
+    }
+
+    lateinit var notificationManager: NotificationManager
+    lateinit var DBHelper : rDBHelper
+    override fun onReceive(context: Context, intent: Intent) {
+        DBHelper = rDBHelper(context)
+        val code = intent.getIntExtra("code", -1)
+        val rid = intent.getIntExtra("rid", -1)
+
+        notificationManager = context.getSystemService(
+            Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        createNotificationChannel()
+        deliverNotification(context, code, rid)
+    }
+
+    private fun deliverNotification(context: Context, code:Int, rid:Int) {
+        val contentIntent = Intent(context, MainActivity::class.java)
+        contentIntent.putExtra("time", code)
+        contentIntent.putExtra("rid", rid)
+
+        val contentPendingIntent = PendingIntent.getActivity(
+            context,
+            code,
+            contentIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        val builder =
+            NotificationCompat.Builder(context, PRIMARY_CHANNEL_ID)
+                .setSmallIcon(R.drawable.memo)
+                .setContentTitle("Alert")
+                .setContentText("조삼..모사..")
+                .setContentIntent(contentPendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+
+        notificationManager.notify(code, builder.build())
+    }
+
+    fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                PRIMARY_CHANNEL_ID,
+                "Stand up notification",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = "AlarmManager Tests"
+            notificationManager.createNotificationChannel(
+                notificationChannel)
+        }
+    }
+}
